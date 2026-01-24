5a18b7390ae12a5996b6fc720493b0094ac307c9
sargentogato <marlonjvelasquez@gmail.com>
2026-01-24 19:18:33 +0100

    update config for github pages and reinstall dependecies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { fileURLToPath } from 'url';  
 import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/drbanuls-inspired-design/',
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
