227f65d5d684abdfcf0627bf089b855ccb2c9269
sargentogato <marlonjvelasquez@gmail.com>
2026-01-24 19:06:32 +0100

    update config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/drbanuls-inspired-design/',
   plugins: [vue()],
   resolve: {
     alias: {
