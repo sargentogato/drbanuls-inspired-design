8be99b4e7574908a2a193951183033dc03d132c4
sargentogato <marlonjvelasquez@gmail.com>
2026-01-24 19:20:43 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath } from 'url';  
 import { defineConfig } from 'vite';
-import vueDevTools from 'vite-plugin-vue-devtools';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/drbanuls-inspired-design/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
        '@': fileURLToPath(new URL('./src', import.meta.url)),
